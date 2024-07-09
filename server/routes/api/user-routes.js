a23391fd7970ce0b6c21fb86263919c25bac4098
jeflynn135 <jenna.flynn135@gmail.com>
2024-07-08 20:54:59 -0700

    backend starters

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  saveReview,
+  deleteReview,
+  login,
+} = require('../../controllers/user-controller');
+
+// import middleware
+const { authMiddleware } = require('../../utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware, saveReview);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/Reviews/:ReviewId').delete(authMiddleware, deleteReview);
+
+module.exports = router;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userInfo = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+        return userInfo;
+      }
+
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveReview: async (parent, { reviewData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedReviews: reviewData } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+    removeReview: async (parent, { reviewId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedReviews: { reviewId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
