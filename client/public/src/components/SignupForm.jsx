c0b3c0234385622959f15694b2730d6d181cf3f9
Casi Clarkson <casiclarkson@gmail.com>
2024-07-09 19:47:02 -0700

    login and signup jsx complete with mutations added in mutations.js

--- client/public/src/components/SignupForm.jsx
+++ client/public/src/components/SignupForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import {useMutation} from "@apollo/client";
+import {ADD_USER} from "../utils/mutations";
 import Auth from "../utils/auth";
+import {Form, Button, Alert} from "react-bootstrap";
 
 const SignUpForm = () => {
     const [userData, setUserData] = useState({
@@ -51,5 +53,78 @@ const SignUpForm = () => {
 
 return (
     //idk what is all this shit
-)
-}
+    <>
+  
+      <Form noValidate validated={validated} onSubmit={submitHandler}>
+     
+        <Alert
+          dismissible
+          onClose={() => showAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
+          Something went wrong with your signup!
+        </Alert>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor="username">Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your username"
+            name="username"
+            onChange={inputHandler}
+            value={userData.username}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Your email address"
+            name="email"
+            onChange={inputHandler}
+            value={userData.email}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Email is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={inputHandler}
+            value={userData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={
+            !(
+              userData.username &&
+              userData.email &&
+              userData.password
+            )
+          }
+          type="submit"
+          variant="success"
+        >
+          Submit
+        </Button>
+      </Form>
+    </>
+);
+};
+export default SignUpForm;
