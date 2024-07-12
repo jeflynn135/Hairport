c3b87a6983276635b57bd664f1af2576249f6996
mbyford49 <mbyford49@gmail.com>
2024-07-11 19:26:43 -0700

    conflict resolution 2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-//const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
